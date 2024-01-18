b7773dde0cea8f5e85e7201bc47798960f5a049e
heeneth <heeneth123@gmail.com>
2024-01-18 17:20:50 +0530

    adding new branch uc1 and new uc1

--- EmployeeWage/EmployeeWageSolution.java
+++ EmployeeWage/EmployeeWageSolution.java
@@ -1,20 +1,37 @@
-// Check Employee is Present or Absent
+// UC1 Check Employee is Present or Absent
+// UC2 Add Part time Employee & Wage
+
 public class EmployeeWageSolution {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
 
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
             System.out.println("Employee is Present");
+            System.out.println("Emp Wage: " + empWage);
 
         } else {
-            System.out.println("Employee is Abscent");
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Employee is Adscent");
+            System.out.println("Emp Wage: " + empWage);
+
         }
     }
 
 }
 
-// [Running] cd "c:\Users\nithi\javaAssignment\EmployeeWage\" && javac
 // EmployeeWageSolution.java && java EmployeeWageSolution
 // Employee is Abscent
 // Employee is Present
+
+// EmployeeWageSolution.java && java EmployeeWageSolution
+// Employee is Present
+// Emp Wage:
+// 160
